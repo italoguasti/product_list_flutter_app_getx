e61f3e81105a236f679d6c3ca89c5f4360acc5f0
Italo Guasti <italo.guasti@aluno.ufop.edu.br>
2022-08-08 11:03:26 -0300

    add getx

---
+++ lib/pages/splash.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 4), () {
+      Get.toNamed('/home');
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SvgPicture.network(
+          'https://usemobile.com.br/wp-content/uploads/2020/07/logo-horizontal-site-branco.svg',
+          width: 120.0,
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
