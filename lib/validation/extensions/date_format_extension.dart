677319cd6c229d5b8996a59d004c59dde0324ee0
Italo Guasti <italo.guasti@aluno.ufop.edu.br>
2022-10-07 13:42:19 -0300

    refactory date format

--- lib/validation/extensions/date_format_extension.dart
+++ lib/validation/extensions/date_format_extension.dart
@@ -1,11 +1,5 @@
 import 'package:intl/intl.dart';
-import 'package:intl/date_symbol_data_local.dart';
 
 extension DateFormatExtension on DateTime {
-  String toDateFormat() {
-    initializeDateFormatting();
-    final format = DateFormat(DateFormat.YEAR_NUM_MONTH_DAY, 'pt_Br').format(this);
-
-    return format;
-  }
+  String toDateFormat() => DateFormat('dd/MM/yyyy').format(this);
 }
